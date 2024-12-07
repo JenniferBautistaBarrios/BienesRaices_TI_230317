ded146d0bf05f136f1865e42673d9f2e83577430
JenniferBautistaBarrios <230317@utxicoetepec.edu.mx>
2024-12-07 14:00:46 -0600

    ACTIVIDAD 06
    En la tarjetas (CARDS) de las propiedades incluir el Avatar del Vendedor y su alias de usuario (*)

--- models/index.js
+++ models/index.js
@@ -4,13 +4,14 @@ import Categoria from './Categoria.js'
 import Usuario from './Usuario.js'
 import Mensaje from './Mensaje.js'
 
-Propiedad.belongsTo(Precio, { foreignKey: 'precioID' })
-Propiedad.belongsTo(Categoria, { foreignKey: 'categoriaID' })
-Propiedad.belongsTo(Usuario, { foreignKey: 'usuarioID' })
-Propiedad.hasMany(Mensaje, { foreignKey: 'propiedadID' })
+Propiedad.belongsTo(Precio, { foreignKey: 'precioID'});
+Propiedad.belongsTo(Categoria, { foreignKey: 'categoriaID' });
+Propiedad.belongsTo(Usuario, { foreignKey: 'usuarioID' });
+Propiedad.hasMany(Mensaje, { foreignKey: 'propiedadID' });
 
-Mensaje.belongsTo(Propiedad, { foreignKey: 'propiedadID' })
-Mensaje.belongsTo(Usuario, { foreignKey: 'usuarioID' })
+Mensaje.belongsTo(Propiedad, { foreignKey: 'propiedadID' });
+Mensaje.belongsTo(Usuario, { foreignKey: 'usuarioID' });
+
 
 
 export {
--- seed/usuarios.js
+++ seed/usuarios.js
@@ -1,8 +1,10 @@
 import bcrypt from 'bcryptjs'
 const usuarios = [
     {
+        alias: 'Jenny',
         nombre: 'Jennifer',
         email: 'Jenny@gmail.com',
+        imagen: 'Jenny.jpg',
         confirmado: 1,
         fechaNacimiento: '2005-05-11',
         password: bcrypt.hashSync('password', 10)
